f76da7ff887d2abea2532027e3e6b96563d4f9a5
Adam <adamhughesme1@gmail.com>
2025-11-03 02:57:19 -0500

    creating stylized window that can drag, minimize, full screen, and close

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,39 @@ import { desktopIcons } from "@/components/desktop/lib/desktopIconsData";
 import { useWindows } from "@/components/window/hooks/useWindows";
 
 export default function Home() {
-  const { windows, openWindow } = useWindows();
+  const {
+    windows,
+    openWindow,
+    closeWindow,
+    minimizeWindow,
+    maximizeWindow,
+    focusWindow,
+    updateWindowPosition,
+    updateWindowSize,
+  } = useWindows();
+
+  const handleIconDoubleClick = (icon: typeof desktopIcons[0]) => {
+    openWindow(icon.id, icon.title, icon.icon, icon.content);
+  };
 
   return (
     <div>
       <Dekstop
         icons={desktopIcons}
-        onIconDoubleClick={(icon) => openWindow(icon.id, icon.title, icon.icon, icon.content)}
+        onIconDoubleClick={(icon) => handleIconDoubleClick(icon)}
       />
   
       {windows.map((window) => (
-        <Window key={window.id} windowData={window} />
+        <Window 
+          key={window.id}
+          windowData={window}
+          closeWindow={() => closeWindow(window.id)}
+          minimizeWindow={() => minimizeWindow(window.id)}
+          maximizeWindow={() => maximizeWindow(window.id)}
+          focusWindow={() => focusWindow(window.id)}
+          updateWindowPosition={(position) => updateWindowPosition(window.id, position)}
+          updateWindowSize={(size) => updateWindowSize(window.id, size)}
+        />
       ))}
     </div>
   );
