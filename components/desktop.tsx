2f0ee48c82afd74548fbc22225c70764cc381a0a
Adam <adamhughesme1@gmail.com>
2025-11-02 22:06:22 -0500

    Update desktop component to enhance styling and layout

--- components/desktop.tsx
+++ components/desktop.tsx
@@ -13,7 +13,10 @@ type DesktopProps = {
 
 export default function Dekstop({ icons }: DesktopProps) {
     return (
-        <div>
+        <div
+          className="flex-1 p-4 reactive"
+          style={{ backgroundColor: "#008080" }}
+        >
             {icons.map((icon) => (
                 <div key={icon.id} style={{ display: 'inline-block', margin: '10px', textAlign: 'center' }}>
                     <div style={{ fontSize: '48px' }}>{icon.icon}</div>
