a9bb05b183637bba11fbf0940b51dc0a5f439231
Adam <adamhughesme1@gmail.com>
2025-11-02 21:45:44 -0500

    Add desktop component to display icons and refactor home page

---
+++ components/desktop.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import type React from "react"
+
+import { useState } from "react"
+
+type DesktopIcon = {
+    id: string
+    title: string
+    icon: string
+    content: React.ReactNode
+}
+
+type DesktopProps = {
+    icons: DesktopIcon[]
+}
+
+export default function Dekstop({ icons }: DesktopProps) {
+    return (
+        <div>
+            {icons.map((icon) => (
+                <div key={icon.id} style={{ display: 'inline-block', margin: '10px', textAlign: 'center' }}>
+                    <div style={{ fontSize: '48px' }}>{icon.icon}</div>
+                    <div>{icon.title}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
